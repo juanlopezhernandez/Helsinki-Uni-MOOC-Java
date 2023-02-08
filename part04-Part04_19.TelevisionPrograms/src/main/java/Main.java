2bd3d6d579ae74b3d1f345c13234d8b969c9bbf9
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-02-07 18:42:49 -0800

    comparing objects from scanner using for-each loop with if statement

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -4,10 +4,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // implement here your program that uses the TelevisionProgram class
-
-        ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<TelevisionProgram> programs = new ArrayList<>();
+        
+        while (true) {
+            System.out.println("Name: ");
+            String show = scanner.nextLine();
+            if (show.isEmpty()){
+                break;
+            }
+            
+            System.out.println("Duration: ");
+            int tiempo = Integer.valueOf(scanner.nextLine());
+            
+            programs.add( new TelevisionProgram(show,tiempo));
+        }
+        
+        System.out.println("Program's maximum duration? ");
+        int maxTime = Integer.valueOf(scanner.nextLine());
+        
+        for (TelevisionProgram length : programs){
+            //remember: if (true) {run};
+            if (maxTime >= length.getDuration()) {
+            System.out.println(length);
+            }   
+        }
     }
 }
