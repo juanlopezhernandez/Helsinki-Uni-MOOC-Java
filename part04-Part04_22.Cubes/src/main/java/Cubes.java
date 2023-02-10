f3197d3a73c985ad742947bbf49efc4ae532f7b4
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-02-09 21:46:06 -0800

    Scanner taking a string and integer ^3

--- part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,14 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            String inputS = scanner.nextLine();
+            if (inputS.equals("end")){
+                break;
+            }
+            int inputN = Integer.valueOf(inputS);
+            System.out.println(inputN*inputN*inputN);
+        }        
     }
 }
