282d748fdac91eca7b0eb94e90be51ccf1e806cd
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-02-11 14:11:53 -0800

    use of Path.get(filename.extension) to read/access different files from code

--- part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class PrintingAFile {
 
     public static void main(String[] args) {
-
+        try (Scanner scan = new Scanner(Paths.get("data.txt"))) {
+            while (scan.hasNextLine()){
+                String linea = scan.nextLine();
+                System.out.println(linea);
+            }
+        } catch (Exception e) {
+            
+        }
+            
     }
 }
