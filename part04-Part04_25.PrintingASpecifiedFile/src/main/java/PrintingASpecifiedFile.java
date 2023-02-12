913731f5f63a1bd8c354f9b6e36b5da9d9b8fea8
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-02-11 19:18:54 -0800

    scanned user input->input used to find file

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,17 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Which file should be printed? ");
+        String fileName = scanner.nextLine();
+        
+        try(Scanner fileScanner = new Scanner(Paths.get(fileName))){
+            while (fileScanner.hasNextLine()){
+                String fileContent = fileScanner.nextLine();
+                System.out.println(fileContent);
+            }
+        } catch (Exception e){
+            //System.out.println(e + "does not exist.");
+        }
 
     }
 }
