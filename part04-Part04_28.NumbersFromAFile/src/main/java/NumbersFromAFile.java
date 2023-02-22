90b25d592631a3589d97cabcac03d22c78f9c8e6
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-02-21 22:28:24 -0800

    used a counter outside try and catch

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,11 +1,14 @@
 
+
 import java.nio.file.Paths;
 import java.util.Scanner;
+
 
 public class NumbersFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int counter=0;
 
         System.out.print("File? ");
         String file = scanner.nextLine();
@@ -13,6 +16,21 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        
+        try(Scanner fileSearch = new Scanner(Paths.get(file))){
+            while (fileSearch.hasNext()){
+                int currentNum =fileSearch.nextInt();
+                //System.out.println(currentNum); to test
+                if(lowerBound<=currentNum && upperBound>=currentNum){
+                    counter++;
+                   // System.out.println(currentNum); to test
+                }
+            }
+            
+            System.out.println("Numbers: " + counter);
+        } catch (Exception e) {
+            
+        }
 
     }
 
