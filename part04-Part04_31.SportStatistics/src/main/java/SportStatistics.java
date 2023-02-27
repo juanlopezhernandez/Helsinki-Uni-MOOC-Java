0ae9a28ac8acc7650e2d6936c5cf885f32adb10d
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-02-26 23:37:39 -0800

    comparing file content with 4 values per line

--- part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
+++ part04-Part04_31.SportStatistics/src/main/java/SportStatistics.java
@@ -7,6 +7,59 @@ public class SportStatistics {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList storage = new ArrayList<>();//would of been of used w/ a method
+        int count = 0;
+        int wins = 0;
+        int loss = 0;
+        System.out.println("File:");
+        String userFile = scan.nextLine();
+        
+        System.out.println("Team:");
+        String userTeam = scan.nextLine();
+        
+        try(Scanner requested = new Scanner (Paths.get(userFile)) ){
+            while (requested.hasNext()){
+                String requestedContent = requested.nextLine();
+                //System.out.println(requestedContent);
+               
+                String [] parts = requestedContent.split(",");
+                String home = parts[0];
+                String away = parts [1];
+                int homeScore = Integer.valueOf(parts[2]);
+                int awayScore = Integer.valueOf(parts[3]);
+                
+                if (userTeam.equals(home)) {
+                    count++;                    
+                    if (homeScore > awayScore ) {
+                        wins++;
+                    } else {
+                        loss++;
+                    }                    
+                    continue;                   
+                }
+                
+                if ( userTeam.equals(away)) {
+                    count++;
+                    if (homeScore > awayScore ) {
+                        loss++;
+                    } else {
+                        wins++;
+                    }                    
+                    continue;                   
+                }
+                
+                if (requestedContent.isEmpty()){
+                    break;
+                }                
+            } 
+            
+            System.out.println("Games: " + count);
+            System.out.println("Wins: " + wins);
+            System.out.println("Losses: " + loss);
+        } catch (Exception e) {
+            
+        }
+        
 
     }
 
