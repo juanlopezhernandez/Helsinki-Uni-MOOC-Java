4c76f0251ae00acb718c768ba23bb58b1b202021
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-03-01 21:13:01 -0800

    Completed part 5, part 1

--- part05-Part05_01.OneMinute/src/main/java/Program.java
+++ part05-Part05_01.OneMinute/src/main/java/Program.java
@@ -5,7 +5,12 @@ public class Program {
 
     public static void main(String[] args) {
         // You can test your program here
-
+      Timer reloj = new Timer();
+      
+      while (true){
+          System.out.println(reloj);
+          reloj.advance();
+      }
 
     }
 }
