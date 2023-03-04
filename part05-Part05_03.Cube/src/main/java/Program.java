dc19b9ddafb462d26a9f76d2b5121bd561df31c2
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-03-03 19:52:07 -0800

    Created a class with constructor and printed

--- part05-Part05_03.Cube/src/main/java/Program.java
+++ part05-Part05_03.Cube/src/main/java/Program.java
@@ -5,6 +5,14 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give me the length and "
+                + "I'll give you the volume of a cube.");
+        int input = Integer.valueOf(scanner.nextLine());
+        
+        Cube userCube = new Cube(input);
+        System.out.println(userCube.volume());
+        System.out.println(userCube);
 
         // Experiment with your program here
     }
