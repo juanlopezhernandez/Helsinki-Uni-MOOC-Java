391fbd0ea2d73a79d90b781f40a17ca93abf9564
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-03-07 19:27:42 -0800

    Overleading Constructors - Multiple constructors diff set of parameters

--- part05-Part05_05.ConstructorOverload/src/main/java/MainProgram.java
+++ part05-Part05_05.ConstructorOverload/src/main/java/MainProgram.java
@@ -2,6 +2,14 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        // Test your class here
+        Product cintaParaMedir = new Product("Measuring Tape");
+        Product plaster = new Product ("Plaster", "isle number 10");
+        Product tyre = new Product ("Tire", 4);
+        
+        System.out.println(cintaParaMedir);
+        System.out.println(plaster);
+        System.out.println(tyre);
+        System.out.println(tyre.getName());
+
     }
 }
