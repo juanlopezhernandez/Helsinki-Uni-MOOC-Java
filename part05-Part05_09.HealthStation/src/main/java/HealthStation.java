664b7757c82ef1fd4d02e00d81fca23dc9eaf7dc
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-03-11 23:58:13 -0800

    objects has parameters in methods/classes

--- part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,26 @@
 
 public class HealthStation {
+    private int numberOfWeighings;
 
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        this.numberOfWeighings++;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person) {
+        person.setWeight(this.weigh(person) + 1);
+    }
+    
+    public int weighings(){
+        return this.numberOfWeighings;
+    }
+    public boolean adult(Person person){
+        if (person.isOfLegalAge()){
+        return true;
+                }
+        return false;
     }
 
 }
--- part05-Part05_09.HealthStation/src/main/java/Main.java
+++ part05-Part05_09.HealthStation/src/main/java/Main.java
@@ -3,5 +3,25 @@ public class Main {
 
     public static void main(String[] args) {
         // write experimental code here to check how your program functions
+        HealthStation childrens = new HealthStation();
+        
+        Person jeff = new Person ("jeff", 19, 50, 200);
+        Person andrew = new Person ("andrew", 5, 25, 143);
+        
+        System.out.println(jeff.getName() + " weighs: " + childrens.weigh(jeff) +" kilos");
+        System.out.println(andrew.getName() + " weighs: " + childrens.weigh(andrew) +" kilos");
+
+        childrens.feed(jeff);        
+        childrens.feed(jeff);
+        
+        System.out.println("");
+        
+        System.out.println(jeff.getName() + " weighs: " + childrens.weigh(jeff) +" kilos");
+
+        System.out.println(childrens.weighings());
+        if (childrens.adult(jeff)){
+              System.out.println(jeff.getName() + " is over 18");
+        }
+      
     }
 }
