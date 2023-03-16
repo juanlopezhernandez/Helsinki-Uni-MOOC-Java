5f5ea3a3254b0a163704bcf44408dac67b3a9860
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-03-15 21:09:05 -0700

    used different objects/classes as parameters, called methods from other objects

--- part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -9,20 +9,61 @@ public class PaymentTerminal {
 
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
+        
     }
 
     public double eatAffordably(double payment) {
-        // an affordable meal costs 2.50 euros
-        // increase the amount of cash by the price of an affordable mean and return the change
-        // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        if (payment < 2.50){
+            return payment;
+        } else {
+            this.money += 2.50;
+            this.affordableMeals ++;
+        }      
+        return payment - 2.50;
     }
 
     public double eatHeartily(double payment) {
+        if (payment < 4.30){
+            return payment;
+        } else {
+            this.money += 4.30;
+            this.heartyMeals++;
+        }
+        return payment - 4.30;
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        
+    }
+    public boolean eatAffordably(PaymentCard card){
+        if (card.balance() < 2.50){
+            return false;
+        } else {
+            card.takeMoney(2.50);
+            this.affordableMeals++;
+        }
+        
+        return true;
+    }
+    
+    public boolean eatHeartily (PaymentCard card){
+        if (card.balance() < 4.30){
+            return false;
+        } else {
+            card.takeMoney(4.30);
+            this.heartyMeals++;
+        }        
+        
+        return true;
+    }
+    
+    public void addMoneyToCard(PaymentCard card, double sum){
+        if (sum >0){
+            this.money += sum;
+            card.addMoney(sum);
+        }
+        
     }
 
 
