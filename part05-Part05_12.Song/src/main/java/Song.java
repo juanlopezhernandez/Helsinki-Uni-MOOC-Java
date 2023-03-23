0106ae18a4b4837e9bf8137f677b5bc995621dfc
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-03-22 20:56:50 -0700

    using the equals method to compare objects

--- part05-Part05_12.Song/src/main/java/Song.java
+++ part05-Part05_12.Song/src/main/java/Song.java
@@ -10,6 +10,21 @@ public class Song {
         this.name = name;
         this.durationInSeconds = durationInSeconds;
     }
+    public boolean equals (Object compared){
+        if (this == compared){
+        return true;
+        } 
+        if (!(compared instanceof Song)){
+            return false;
+        }
+        Song comparedSong = (Song) compared;
+        if (this.name.equals(comparedSong.name) &&
+            this.artist == comparedSong.artist &&
+            this.durationInSeconds == comparedSong.durationInSeconds) {
+            return true;
+        }
+        return false;
+    }
 
     @Override
     public String toString() {
