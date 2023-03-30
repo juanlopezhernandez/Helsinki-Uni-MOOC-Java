6b8e83b007680b0782d09947adedab20d37738d0
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-03-29 22:11:25 -0700

    created equals method, if statement libros.contain(book)

--- part05-Part05_14.Books/src/main/java/Main.java
+++ part05-Part05_14.Books/src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Book> books = new ArrayList<>();
+        ArrayList<Book> libros = new ArrayList<>();
 
         while (true) {
             System.out.println("Name (empty will stop):");
@@ -17,12 +17,17 @@ public class Main {
 
             System.out.println("Publication year:");
             int publicationYear = Integer.valueOf(scanner.nextLine());
+            
             Book book = new Book(name, publicationYear);
-            books.add(book);
-
+            
+            if(libros.contains(book)){
+            System.out.println("The book is already on the list. Let's not add the same book again.");
+            } else {
+            libros.add(book);    
+            }
         }
         
         // NB! Don't alter the line below!
-        System.out.println("Thank you! Books added: " + books.size());
+        System.out.println("Thank you! Books added: " + libros.size());
     }
 }
