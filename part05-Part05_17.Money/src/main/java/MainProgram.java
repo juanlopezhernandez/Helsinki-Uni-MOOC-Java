92a4e73fdf7e6e25e3a2190d559d9305ff6873b4
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-04-05 17:41:21 -0700

    called object within an object independent of each other

--- part05-Part05_17.Money/src/main/java/MainProgram.java
+++ part05-Part05_17.Money/src/main/java/MainProgram.java
@@ -3,5 +3,19 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // test your class here
+        Money a = new Money(10,0);
+        Money b = new Money(3,0);
+        Money g = new Money(5,0);
+
+        Money c = a.plus(b);
+
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 5.00e
+        System.out.println(c);
+        
+        System.out.println(a.lessThan(b));
+        System.out.println(b.lessThan(g));
+        
+        
     }
 }
--- part05-Part05_17.Money/src/main/java/Money.java
+++ part05-Part05_17.Money/src/main/java/Money.java
@@ -14,7 +14,29 @@ public class Money {
         this.euros = euros;
         this.cents = cents;
     }
-
+    
+    public Money plus(Money addition){
+        int whole = this.euros+ addition.euros;
+        int centavo = this.cents+ addition.cents;
+        Money newMoneyPlus = new Money(whole, centavo );
+                
+        return newMoneyPlus;
+    }
+    public Money minus(Money decreaser){
+        int whole = this.euros - decreaser.euros;
+        int centavo = this.cents - decreaser.cents;
+        if(centavo < 0){
+            whole--;
+            centavo= centavo + 100;
+        }
+        if (whole<0){
+            whole = 0;
+            centavo= 0;
+        }
+        
+        Money newMoneyMinus = new Money(whole, centavo);
+        return newMoneyMinus;
+    }
     public int euros() {
         return this.euros;
     }
@@ -22,6 +44,18 @@ public class Money {
     public int cents() {
         return this.cents;
     }
+    
+    public boolean lessThan(Money compared){
+        if(this.euros < compared.euros){
+            return true;
+        }
+        if(this.euros == compared.euros && this.cents < compared.cents){
+            return true;
+        }
+        return false;
+    }
+    
+
 
     public String toString() {
         String zero = "";
