188e0356c12a342ec57f70f422796761a1b8dd00
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-05-01 21:19:17 -0700

    review using arraylists with objects

--- part06-Part06_01.Menu/src/main/java/Main.java
+++ part06-Part06_01.Menu/src/main/java/Main.java
@@ -1,21 +1,26 @@
 
 public class Main {
     public static void main(String[] args) {
-          Menu exactum = new Menu();
+          Menu elMenu = new Menu();
         
         // When you have completed the method addMeal(String meal)
         // You can delete the comments below
         
 
-//        exactum.addMeal("Smoked salmon, white wine and butter sauce with basil");
-//        exactum.addMeal("Seasonal green salad with apple-honey vinegarette");
-//        exactum.addMeal("Roasted lamb in a red wine sauce");
+        elMenu.addMeal("Smoked salmon, white wine and butter sauce with basil");
         
-        // When you have completed the method printMeals()
-        // you can remove the comment below
+        elMenu.addMeal("Seasonal green salad with apple-honey vinegarette");
+        elMenu.addMeal("Roasted lamb in a red wine sauce");
         
-//        exactum.printMeals();
+        elMenu.printMeals();
+        System.out.println("");
+        elMenu.clearMenu();
+        elMenu.addMeal("Una Carnita Asada");        
+        elMenu.printMeals();
         
+        System.out.println("");
+        
+        elMenu.printMeals();
         // When you have completed the method clearMenu()
         // you can remove the comments below
 //        exactum.clearMenu();
--- part06-Part06_01.Menu/src/main/java/Menu.java
+++ part06-Part06_01.Menu/src/main/java/Menu.java
@@ -9,8 +9,22 @@ public class Menu {
         this.meals = new ArrayList<>();
     }
     public void addMeal(String meal){
+        if(!(meals.contains(meal))){
+        // if the <meals> list DOES NOT contain the parameter meal add parameter   
         this.meals.add(meal);
+        }
+    
     }
-
+    public void printMeals(){
+        for (int i = 0; i < meals.size(); i++){
+            System.out.println(this.meals.get(i));        
+            }
+        //return "add meal";
+   }    
+   public void clearMenu (){
+       meals.clear();
+   }
+   
     // implement the required methods here
+
 }
