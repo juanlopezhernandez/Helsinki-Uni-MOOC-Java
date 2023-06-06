acfe0210623aa16b6d44b9aec6902fe894f70317
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-06-05 19:32:50 -0700

    printing from xmen collection

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -13,10 +13,30 @@ public class SimpleCollection {
 
     public void add(String element) {
         this.elements.add(element);
+
     }
 
     public ArrayList<String> getElements() {
         return this.elements;
     }
-    
+
+    public String toString() {
+        String toBePrinted = "The collection " + this.name + " ";
+
+        if (elements.isEmpty()) {
+            return toBePrinted + "is empty.";
+        }
+
+        String charactersinList = "";
+        for (String stringX : elements) {
+            charactersinList = charactersinList + "\n" + stringX;
+        }
+
+        if (elements.size() == 1) {
+            return toBePrinted + "has " + elements.size() + " element:" + charactersinList;
+        } else {
+            return toBePrinted + "has " + elements.size() + " elements:" + charactersinList;
+
+        }
+    }
 }
