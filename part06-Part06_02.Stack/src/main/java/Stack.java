13491500a2bb376adb5d6e52de07c1b2ac273613
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-06-23 18:50:53 -0700

    update to the isEmpty method

--- part06-Part06_02.Stack/src/main/java/Stack.java
+++ part06-Part06_02.Stack/src/main/java/Stack.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  */
 public class Stack {
     
-    private ArrayList<String> contents;
+    private final ArrayList<String> contents;
     
     public Stack (){
         this.contents = new ArrayList<>();
