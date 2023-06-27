bd909dd0536037fe9199a2cbce8269a2556c4d65
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-06-26 20:39:59 -0700

    updated notes for clearity

--- part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
+++ part06-Part06_04.PrintingACollection/src/main/java/SimpleCollection.java
@@ -1,4 +1,4 @@
-
+  
 import java.util.ArrayList;
 
 public class SimpleCollection {
@@ -21,17 +21,26 @@ public class SimpleCollection {
     }
 
     public String toString() {
+        //part below is to keep the code that will be reused in futher in the code
         String toBePrinted = "The collection " + this.name + " ";
-
+        
+        //this part is for an empty arrayList using the repeated code above
         if (elements.isEmpty()) {
             return toBePrinted + "is empty.";
         }
 
+        //empty string to store elements reached with the for loop
         String charactersinList = "";
+        //variable stringX of String type; 
+        //variable type has to be the same as the list you are traversing;
+        //1 index at a time is assigned to the variable
         for (String stringX : elements) {
+            //variable is added to the String created outside the loop.
             charactersinList = charactersinList + "\n" + stringX;
         }
-
+        
+        //code to print list with one element, has to print 'singular'
+        //everything ELSE is plural
         if (elements.size() == 1) {
             return toBePrinted + "has " + elements.size() + " element:" + charactersinList;
         } else {
