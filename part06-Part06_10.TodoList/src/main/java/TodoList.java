9339c22864d40e156e7a5239ba8c39d9e5839d6f
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-07-20 21:25:26 -0700

    creating methods; no functionality yet

---
+++ part06-Part06_10.TodoList/src/main/java/TodoList.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author juanlopez
+ */
+public class TodoList {
+    private ArrayList list;
+    
+    public TodoList(){
+        this.list = new ArrayList <>();
+    }
+    
+}
