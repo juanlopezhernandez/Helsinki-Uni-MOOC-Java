71a61fd139446e4a75bec8dad725b1d7cd8c256a
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-10-18 23:11:47 -0700

    contains unit test added to test class

--- part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,28 @@
 
+import java.util.ArrayList;
+
+
 public class ExerciseManagement {
+    
+    public ArrayList<String> exercises;
+    
+    public ExerciseManagement () {
+        this.exercises = new ArrayList<>();
+    }
+    
+    public ArrayList<String> exerciseList(){
+        return this.exercises;
+    }
+    
+    public void add (String exercise){
+        this.exercises.add("Write a test");
+    }
+    
+//    public boolean contains (String exercise) {
+//        if (this.exercises.contains(exercise)){
+//            return true;
+//        }
+//        return false;
+//    }
 
 }
--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,4 +1,6 @@
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 
@@ -8,7 +10,24 @@ public class ExerciseManagementTest {
     @Test
     public void exerciseListEmptyAtBeginning () {
         ExerciseManagement management = new ExerciseManagement();
+        //           parameter 1 is what is EXPECTED (0),
+        //           parameter 2 is what management.exerciseList().size() ACTUALly is
+        //           (expected,actual)
         assertEquals (0,management.exerciseList().size());
+        
+    }
+    //in this test we are checking if the list grows by 1 when 'add' method is called
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        ExerciseManagement management = new ExerciseManagement();
+        management.add("Write a test");
+        assertEquals (1, management.exerciseList().size());
+    }
+    @Test
+    public void addedExerciseIsInList () {
+        ExerciseManagement management = new ExerciseManagement();
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
         
     }
 
