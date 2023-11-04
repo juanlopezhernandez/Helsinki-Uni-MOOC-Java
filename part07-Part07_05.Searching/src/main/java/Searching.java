6d4d195829a60e84e466b353c36e49fba6be2b57
juanlopezhernandez <lopezjuan0192@gmail.com>
2023-11-03 17:09:28 -0700

    binary search code in progress attemping a 'for' loop, not functional yet'

--- part07-Part07_05.Searching/src/main/java/Searching.java
+++ part07-Part07_05.Searching/src/main/java/Searching.java
@@ -1,5 +1,8 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Searching {
@@ -54,6 +57,28 @@ public class Searching {
     }
 
     public static int binarySearch(ArrayList<Book> books, long searchedId) {
+        //sorts the arrayList provided, can still use the 'books'ArrayList type Book
+        Collections.sort(books, Comparator.comparing(Book::getId));
+        
+        int start = 0;
+        int end = books.size()-1;
+        
+        for (int i = start; i <books.size()-1; i++){
+           while (start < end){
+            int middle = (start - end) /2;
+           
+            if (books.get(middle).getId()== searchedId){
+                return middle;
+            }
+            if (books.get(middle).getId()< searchedId){
+                start = middle +1;
+            }
+            
+            if (books.get(middle).getId() > searchedId){
+                end = middle +1;
+            }
+        }
+        }
         return -1;
     }
 }
